56dc4da75274373703ce1c8bfbfcfc407c9c3137
Ferry <ferryhermawan864@gmail.com>
2019-11-28 21:31:04 +0700

    Tugas 8 JS

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function panggilKaryawan(){
+  var karyawan = {
+    nama : 'Budi',
+    gender : 'Pria',
+    umur : 20,
+    jabatan : 'Karyawan',
+  }
+  console.log(karyawan);
+  karyawan.gaji = 2000000;
+  console.log(karyawan);
+}
+panggilKaryawan();
